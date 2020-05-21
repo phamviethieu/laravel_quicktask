da50d5ba1c30e23355daef88deb9ed7ec2bc11f8
phamviethieu <pviethieu@gmail.com>
2020-05-21 14:53:47 +0700

    add template, create validate

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,18 +7,16 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\TaskRequest;
+use Illuminate\Support\Facades\Session;
 
 class TaskController extends Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $user = Auth::user();
@@ -33,69 +31,55 @@ class TaskController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        $vld = $this->validate($request,[
-                'name' => 'required',
-            ],[
-                'name.required'=>'Tên task không được để trống',
-            ]);
         $task['name'] = $request->input('name');
         $task['user_id'] = $request->user()->id;
         Task::create($task);
+        Session::flash('message', trans('message.added')); 
+        Session::flash('alert-class', 'alert-success'); 
 
         return redirect()->route('tasks.index');
     }
 
     public function edit($id)
     {
-        $task = Task::find($id);
-        if(isset($task)){
-            return view('update',[
-                'task'=>$task,
+        try {
+            $task = Task::findOrFail($id);
+        
+            return view('update', [
+                'task' => $task,
             ]);
+        } catch (ModelNotFoundException $e) {
+            
+            return view('404');
         }
-        else return view('404');
         
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(TaskRequest $request, $id)
     {
-        $vld = $this->validate($request,[
-                'name' => 'required',
-            ]);
         $task = Task::find($id);
         $task->name = $request->name;
         $task->save();
-
+        Session::flash('message', trans('message.updated')); 
+        Session::flash('alert-class', 'alert-success'); 
+        
         return redirect()->route('tasks.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $task = Task::find($id);
-        $task->delete();
+        try {
+            $task = Task::findOrFail($id);
+            $task->delete();
+            Session::flash('message', trans('message.deleted')); 
+            Session::flash('alert-class', 'alert-success'); 
+            return redirect()->route('tasks.index');
+        } catch (ModelNotFoundException $e) {
 
-        return redirect()->route('tasks.index');
+            return view('404');
+        }
     }
 
     /**
@@ -103,7 +87,7 @@ class TaskController extends Controller
      */
     public function changeLanguage($language)
     {
-        \Session::put('website_language', $language);
+        Session::put('website_language', $language);
 
         return redirect()->back();
     }
